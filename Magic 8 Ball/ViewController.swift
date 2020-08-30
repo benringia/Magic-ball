d485594aaca0a3ba4261d8140d09498d8be5745a
Ben Ringia <benringia@gmail.com>
2020-08-30 19:31:02 +0800

    randomize

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,17 +2,23 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Ben Ringia on 08/30/2020.
+//  Copyright © 2020 Ben Clauser. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball5")]
 
 
-
+    @IBOutlet weak var ballAnswer: UIImageView!
+    
+    
+    @IBAction func askButton(_ sender: Any) {
+        ballAnswer.image = ballArray.randomElement()
+    }
+    
 }
 
